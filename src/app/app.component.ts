4c902419b106da410e18b15e2977d363ce7dfe0d
Dhananjayahd <dhananjayahd296@gmail.com>
2021-02-12 18:30:04 +0530

    1st version of App!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,66 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe';
+  
+  winMessage : string = '';
+  isCross = false;
+  itemArray : string[] = new Array(9).fill('empty');
+
+  handleClick = (index : number) => {
+    if(this.winMessage){
+      return this.toastr.success(this.winMessage)
+    }
+
+    if(this.itemArray[index] == 'empty'){
+      this.itemArray[index] = (this.isCross) ? 'cross' : 'circle';
+      this.isCross = !this.isCross; 
+    }else{
+      return this.toastr.info("Already Filled!!")
+    }
+
+    this.checkWinner();
+  }
+
+  resetGame = () =>{
+    this.winMessage = '';
+    this.isCross = false;
+    this.itemArray = new Array(9).fill('empty');
+  }
+
+  checkWinner = () => {
+    if(this.itemArray[0] != 'empty' && this.itemArray[0] == this.itemArray[1] && 
+    this.itemArray[0] == this.itemArray[2]){
+      this.winMessage = `${this.itemArray[0]} won!!`
+    }
+    else if(this.itemArray[3] != 'empty' && this.itemArray[3] == this.itemArray[4] && 
+    this.itemArray[3] == this.itemArray[5]){
+      this.winMessage = `${this.itemArray[3]} won!!`
+    }
+    else if(this.itemArray[6] != 'empty' && this.itemArray[6] == this.itemArray[7] && 
+    this.itemArray[6] == this.itemArray[8]){
+      this.winMessage = `${this.itemArray[6]} won!!`
+    }
+    else if(this.itemArray[0] != 'empty' && this.itemArray[0] == this.itemArray[3] && 
+    this.itemArray[0] == this.itemArray[6]){
+      this.winMessage = `${this.itemArray[0]} won!!`
+    }
+    else if(this.itemArray[1] != 'empty' && this.itemArray[1] == this.itemArray[4] && 
+    this.itemArray[1] == this.itemArray[7]){
+      this.winMessage = `${this.itemArray[1]} won!!`
+    }
+    else if(this.itemArray[2] != 'empty' && this.itemArray[2] == this.itemArray[5] && 
+    this.itemArray[2] == this.itemArray[8]){
+      this.winMessage = `${this.itemArray[2]} won!!`
+    }
+    else if(this.itemArray[0] != 'empty' && this.itemArray[0] == this.itemArray[4] && 
+    this.itemArray[0] == this.itemArray[8]){
+      this.winMessage = `${this.itemArray[0]} won!!`
+    }
+    else if(this.itemArray[2] != 'empty' && this.itemArray[2] == this.itemArray[4] && 
+    this.itemArray[2] == this.itemArray[6]){
+      this.winMessage = `${this.itemArray[2]} won!!`
+    }
+  }
+
+  constructor(private toastr : ToastrService){}
 }
